9648a28b1929725120441a227f58f12f8703336d
Deane <d4rracuda@ya.ru>
2019-09-15 00:32:54 +0300

    alert dialog

--- app/src/main/java/com/lydone/sharebillandroid/main/MainFragment.kt
+++ app/src/main/java/com/lydone/sharebillandroid/main/MainFragment.kt
@@ -1,15 +1,57 @@
 package com.lydone.sharebillandroid.main
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.lydone.sharebillandroid.architecture.fragments.FragmentWithViewModelAndNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 class MainFragment : FragmentWithViewModelAndNavController<MainViewModel>() {
     override val viewModelClass: Class<MainViewModel> = MainViewModel::class.java
     override val layout: Int = com.lydone.sharebillandroid.R.layout.fragment_main
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener{
+            showDialog()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity as AppCompatActivity).supportActionBar!!.title = "Главная"
+    }
+    private fun showDialog(){
+        lateinit var dialog:AlertDialog
+        val array = arrayOf("Русалочка","Барракуда","Мыльница","Ремесло")
+        val builder = context?.let { AlertDialog.Builder(it) }
+        builder?.setTitle("Создать группу")
+
+        builder?.setPositiveButton(android.R.string.no) { dialog, which ->
+            Toast.makeText(getContext(),
+                "окей", Toast.LENGTH_SHORT).show()
+        }
+
+        builder?.setNegativeButton(android.R.string.no) { dialog, which ->
+            Toast.makeText(getContext(),
+                "назад", Toast.LENGTH_SHORT).show()
+        }
+
+        builder?.setSingleChoiceItems(array,-1) { _, which->
+            val groupName = array[which]
+            try {
+                Toast.makeText(context,"$groupName color selected.",Toast.LENGTH_LONG).show()
+            }catch (e:IllegalArgumentException){
+                Toast.makeText(context,"$groupName cannot be selected.",Toast.LENGTH_LONG).show()
+            }
+        }
+
+        if (builder != null) {
+            dialog = builder.create()
+        }
+
+        dialog.show()
     }
 }
