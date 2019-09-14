e33444478cf2cd32bc772fe540924738510de2c0
Valerii Belaventsev <vebelaventsev@gmail.com>
2019-09-15 01:18:28 +0300

    Fix

--- app/src/main/java/com/lydone/sharebillandroid/architecture/viewmodels/ViewModelFactory.kt
+++ app/src/main/java/com/lydone/sharebillandroid/architecture/viewmodels/ViewModelFactory.kt
@@ -2,7 +2,6 @@ package com.lydone.sharebillandroid.architecture.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.lydone.sharebillandroid.application.test.TestViewModel
 import com.lydone.sharebillandroid.awards.AwardsViewModel
 import com.lydone.sharebillandroid.history.HistoryViewModel
 import com.lydone.sharebillandroid.main.MainViewModel
@@ -13,9 +12,6 @@ import javax.inject.Inject
 class ViewModelFactory @Inject constructor(private val repository: Repository) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return when {
-            modelClass.isAssignableFrom(TestViewModel::class.java) -> {
-                TestViewModel(repository) as T
-            }
             modelClass.isAssignableFrom(MainViewModel::class.java) -> {
                 MainViewModel(repository) as T
             }
